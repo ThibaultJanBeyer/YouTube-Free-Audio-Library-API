0d4e5b57185b5434aca89932f3d6d17011e0eb47
Thibault Jan Beyer <thibault.beyer@gmail.com>
2020-06-08 20:54:21 +0200

    add folder 1

---
+++ index.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import songs1 from './songs/1.js'
+//import songs2 from './songs/2.js'
+//import songs3 from './songs/3.js'
+
+const url = 'http://docs.google.com/uc?export=open&id='
+
+const all = [...songs1, ...songs2, ...songs3]
+const arr = []
+const map = {}
+
+all.map(({id}) => { map[id] = `${url}${id}`; arr.push(id); })
+
+const res = {
+  arr,
+  map: map,
+  count: arr.length,
+}
+
+fs.writeFileSync(`./info.json`, JSON.stringify(res))
---
+++ readme.md
@@ -0,0 +1,5 @@
+# YouTube Free Music Library API
+
+All the songs from the Youtube Audio Library (https://www.youtube.com/audiolibrary/music?nv=1) accessible via API GET endpoints
+
+Endpoint: ./info.json
