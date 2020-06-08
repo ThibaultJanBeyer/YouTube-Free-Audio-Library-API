fac60238cef3f1544463276ec3f15c44275e2b51
Thibault Jan Beyer <thibault.beyer@gmail.com>
2020-06-08 20:58:07 +0200

    add all to api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import songs1 from './songs/1.js'
-//import songs2 from './songs/2.js'
-//import songs3 from './songs/3.js'
+import songs2 from './songs/2.js'
+import songs3 from './songs/3.js'
 
 const url = 'http://docs.google.com/uc?export=open&id='
 
@@ -15,6 +15,7 @@ const res = {
   arr,
   map: map,
   count: arr.length,
+  all,
 }
 
 fs.writeFileSync(`./api.json`, JSON.stringify(res))
